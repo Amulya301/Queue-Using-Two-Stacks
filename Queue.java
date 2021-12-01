7520eaaa437b796571454206dc86dd8c35b5619d
AmulyaVishnbhotla <64000166+AmulyaVishnbhotla@users.noreply.github.com>
2021-12-01 11:38:35 +0530

    added time and space complexities

--- Queue.java
+++ Queue.java
@@ -16,11 +16,13 @@ class Queue {
     }
 
     // Push element x to the back of queue.
+    // time complexity: O(1), space complexity:O(n)
     public void enqueue(int x) {
         stack1.push(x);
     }
 
     // Removes the element from in front of queue.
+    // time complexity: O(1), space complexity:O(n)
     public int dequeue() {
         peek();
         return stack2.pop();
@@ -28,6 +30,7 @@ class Queue {
     }
     
     // Get the front element.
+    // time complexity: O(n), space complexity:O(n)
     public int peek() {
         if(stack2.isEmpty()){
            while(!stack1.isEmpty()){
@@ -39,7 +42,7 @@ class Queue {
     }
     
     // Return whether the queue is empty.
-    // time complexity: O(1), space complexity: O(1)
+    // time complexity: O(1), space complexity: O(n)
     public boolean empty() {
         return stack1.isEmpty() && stack2.isEmpty();
     }
